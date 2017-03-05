ed9f7e3165f855046fc5af850a2bbe5388298c41
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-03-05 20:51:20 +0100

    Add Vollkorn font and new colours

--- js/HomePage.js
+++ js/HomePage.js
@@ -5,8 +5,19 @@ const HomePage = React.createClass({
     return (
       <div className='HomePage'>
         <header>
-          <h1>words<span>.today</span></h1>
+          <nav>
+            <ul>
+              <li><a href='#'>About</a></li>
+              <li><a href='#'>Contact</a></li>
+              <li><a href='#'>Sign in</a></li>
+            </ul>
+          </nav>
         </header>
+        <section className='HomePage__quote'>
+          <p>„Learn everything you can, anytime you can, from anyone you can.</p>
+          <p>There will always come a time when you will be grateful you did.”</p>
+          <p>— Sarah Caldwell</p>
+        </section>
       </div>
     )
   }
