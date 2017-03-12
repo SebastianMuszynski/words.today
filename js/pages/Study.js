c6804eb8112f0a5814d9f383855597123b053e33
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-03-12 21:26:46 +0100

    Fix scrolling, add words categories

--- js/pages/Study.js
+++ js/pages/Study.js
@@ -6,31 +6,30 @@ const Study = React.createClass({
     return (
       <div className='Study'>
         <section className='sets'>
-          <div className='sets__set'>
-            HOME
-          </div>
-          <div className='sets__set'>
-            JOBS
-          </div>
-          <div className='sets__set'>
-            SCHOOL
-          </div>
-          <div className='sets__set'>
-            WORK
-          </div>
+          <div className='sets__set'>JOBS</div>
+          <div className='sets__set'>SCHOOL</div>
+          <div className='sets__set'>CALENDAR</div>
+          <div className='sets__set'>WORK</div>
 
-          <div className='sets__set'>
-            HOME
-          </div>
-          <div className='sets__set'>
-            JOBS
-          </div>
-          <div className='sets__set'>
-            SCHOOL
-          </div>
-          <div className='sets__set'>
-            WORK
-          </div>
+          <div className='sets__set'>ANIMALS</div>
+          <div className='sets__set'>BODY</div>
+          <div className='sets__set'>COSMETICS</div>
+          <div className='sets__set'>NEWSPAPER</div>
+
+          <div className='sets__set'>BUILDINGS</div>
+          <div className='sets__set'>CARS</div>
+          <div className='sets__set'>HOME</div>
+          <div className='sets__set'>CELEBRATIONS</div>
+
+          <div className='sets__set'>CLOTHES</div>
+          <div className='sets__set'>COLORS</div>
+          <div className='sets__set'>COMPUTERS</div>
+          <div className='sets__set'>COUNTRIES</div>
+
+          <div className='sets__set'>FAMILY</div>
+          <div className='sets__set'>DRINKS</div>
+          <div className='sets__set'>FOOD</div>
+          <div className='sets__set'>GEOGRAPHY</div>
         </section>
       </div>
     )
