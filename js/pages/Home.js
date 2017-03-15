197b73799255f848cef524765d2dda00de3994e3
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-03-15 17:27:20 +0100

    Redesign welcome heading and animations

--- js/pages/Home.js
+++ js/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import '../../css/Home'
 
 const Home = React.createClass({
@@ -7,6 +8,10 @@ const Home = React.createClass({
       <div className='Home'>
         <section className='info'>
           <h2>LEARN NEW<br/> <span>WORDS.TODAY</span></h2>
+          <div className='auth-links'>
+            <Link to='/login'>Log in</Link>
+            <Link to='/signup'>Sign up</Link>
+          </div>
         </section>
       </div>
     )
