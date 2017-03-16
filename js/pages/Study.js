3ac88786e1b242d7484548a42cae76b8bcc72d08
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-03-16 16:34:35 +0100

    Generate and verify answers

--- js/pages/Study.js
+++ js/pages/Study.js
@@ -17,6 +17,50 @@ const Study = React.createClass({
       this.setState({ words: shuffleArray(words) })
     })
   },
+  getWordAnswerRef (wordIndex, answerIndex) {
+    return `word_${wordIndex}_answer_${answerIndex}`
+  },
+  generateAnswers (wordIndex) {
+    return (
+      <div className='translations'>
+        {this.getAnswersForWord(wordIndex).map((answer, index) => {
+          return (
+            <div
+              key={index}
+              ref={this.getWordAnswerRef(wordIndex, index)}
+              className='translations__translation'
+              onClick={this.handleCheckAnswer.bind(this, wordIndex, answer, index)}
+            >
+              {answer}
+            </div>
+          )
+        })}
+      </div>
+    )
+  },
+  getAnswersForWord (wordIndex) {
+    const word = this.state.words[wordIndex]
+    let answers = [word.translations[0].name]
+    while (answers.length < 4) {
+      let index = Math.floor(Math.random() * this.state.words.length)
+      let randomWord = this.state.words[index]
+      answers.push(randomWord.translations[0].name)
+    }
+    return shuffleArray(answers)
+  },
+  handleCheckAnswer (wordIndex, answer, answerIndex) {
+    let isCorrect = false
+    const word = this.state.words[wordIndex]
+    const translations = word.translations
+    for (let i = 0; i < translations.length && !isCorrect; i++) {
+      isCorrect = !!(translations[i].name == answer)
+    }
+    this.highlightAnswer(isCorrect, wordIndex, answerIndex)
+  },
+  highlightAnswer (isCorrect, wordIndex, answerIndex) {
+    const ref = this.getWordAnswerRef(wordIndex, answerIndex)
+    this.refs[ref].classList.add(isCorrect ? 'correct' : 'incorrect')
+  },
   render () {
     return (
       <div className='StudyPage'>
@@ -24,26 +68,13 @@ const Study = React.createClass({
           <h1>Study</h1>
           <div>
             <div>
-              {this.state.words.map((word) => {
+              {this.state.words.map((word, index) => {
                 return (
                   <div key={word.name}>
                     <div className='word'>
                       {word.name}
                     </div>
-                    <div className='translations'>
-                      <div className='translations__translation'>
-                        {word.translations[0].name}
-                      </div>
-                      <div className='translations__translation'>
-                        {word.translations[0].name}
-                      </div>
-                      <div className='translations__translation'>
-                        {word.translations[0].name}
-                      </div>
-                      <div className='translations__translation'>
-                        {word.translations[0].name}
-                      </div>
-                    </div>
+                    {this.generateAnswers(index)}
                   </div>
                 )
               })}
